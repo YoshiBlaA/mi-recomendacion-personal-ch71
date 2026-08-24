d3319eff4e2603df345cd833dc7635dfa57026a9
YoshiBlaA <corts.bombo@gmail.com>
2026-08-23 19:52:25 -0600

    Modificar el mensaje de bienvenida .js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-const nombre = prompt("Escribe tu nombre: ");
-const apellido = prompt("Escribe tu primer apellido");
+let nombre = prompt("Escribe tu nombre: ");
+let apellido = prompt("Escribe tu primer apellido");
 const noCancionesAgregadas = 20;
 const annioActual = 2026;
 const acceso = true;
 
 if(acceso){
-    alert(`Bienvenid@ ${nombre} ${apellido}`);
+    if(nombre && apellido){
+        alert(`Bienvenid@ ${nombre} ${apellido}`);
+    }
+    else if(!nombre && apellido){
+        alert(`Bienvenid@ ${apellido}`);
+    }
+    else if(nombre && !apellido){
+        alert(`Bienvenid@ ${nombre}`);
+    }
+    else{
+        alert(`Bienvenido usuario anónimo`);
+    }
+    
     alert(`Durante este año ${annioActual} se han agregado ${noCancionesAgregadas} canciones`)
     alert(`Si tienes dudas sobre cómo usar la página, puedes hacer click sobre el botón de Ayuda`);
     alert(`Que te diviertas :)`);
