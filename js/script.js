8b19e11b2b4c8dddcbdf089b6d3c38566ba39524
YoshiBlaA <corts.bombo@gmail.com>
2026-08-23 21:56:26 -0600

    Eliminar alerts innecesarios js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
  * * VARIABLES DE NOMBRE, APELLIDO, NUMERO DE CANCIONES AGREGADAS EN EL ANNIO ACTUAL Y ACCESO
  */ 
 
-const nombre = prompt("Escribe tu nombre: ");
-const apellido = prompt("Escribe tu primer apellido");
+const nombre = prompt("Escribe tu nombre de usuario: ");
+const bienvenida = "Bienvenid@";
 const noCancionesAgregadas = 20;
 const annioActual = 2026;
 const acceso = true;
@@ -16,24 +16,14 @@ const acceso = true;
 if(acceso){
 
     // * IMPRIMIT NOMBRE Y APELLIDO SI SE PROPORCIONARON
-    if(nombre && apellido){
-        alert(`Bienvenid@ ${nombre} ${apellido}`);
-    }
-    // * SOLO IMPRIMIR APELLIDO SI NO SE PROPORCIONO NOMBRE
-    else if(!nombre && apellido){
-        alert(`Bienvenid@ ${apellido}`);
-    }
-    // * SOLO IMPRIMIR NOMBRE SI NO SE PROPORCIONO APELLIDO
-    else if(nombre && !apellido){
-        alert(`Bienvenid@ ${nombre}`);
+    if(nombre){
+        alert(`${bienvenida} ${nombre} ${apellido}`);
     }
     // * IMPRIMIR USUARIO ANONIMO SI NO SE PROPORCIONO NIGUNO
     else{
-        alert(`Bienvenido usuario anónimo`);
+        alert(`${bienvenida} usuario anónimo`);
     }
     
     // * IMPRIMIR MENSAJES POST-BIENVENIDA, USANDO LAS VARIABLES RESTANTES
-    alert(`Durante este año ${annioActual} se han agregado ${noCancionesAgregadas} canciones`)
-    alert(`Si tienes dudas sobre cómo usar la página, puedes hacer click sobre el botón de Ayuda`);
-    alert(`Que te diviertas :)`);
+    alert(`Durante este año ${annioActual} se han agregado ${noCancionesAgregadas} canciones \nSi tienes dudas sobre cómo usar la página, puedes hacer click sobre el botón de Ayuda`);
 }
